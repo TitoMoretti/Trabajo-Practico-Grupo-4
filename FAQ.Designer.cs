7b2eeb301c66745d350e58d2810a47650afc2ebf
Tito <ernestojmoretti@gmail.com>
2022-08-06 12:13:50 -0300

    Cambios en el form FAQ.cs

--- FAQ.Designer.cs
+++ FAQ.Designer.cs
@@ -47,11 +47,10 @@
             this.PreguntasBox.Controls.Add(this.lblRespuesta1);
             this.PreguntasBox.Controls.Add(this.lblPregunta1);
             this.PreguntasBox.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PreguntasBox.Location = new System.Drawing.Point(11, 60);
-            this.PreguntasBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PreguntasBox.Location = new System.Drawing.Point(8, 49);
+            this.PreguntasBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PreguntasBox.Name = "PreguntasBox";
-            this.PreguntasBox.Padding = new System.Windows.Forms.Padding(4);
-            this.PreguntasBox.Size = new System.Drawing.Size(604, 258);
+            this.PreguntasBox.Size = new System.Drawing.Size(453, 210);
             this.PreguntasBox.TabIndex = 0;
             this.PreguntasBox.TabStop = false;
             this.PreguntasBox.Text = "Preguntas Frecuentes";
@@ -59,9 +58,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 181);
+            this.label1.Location = new System.Drawing.Point(14, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(445, 60);
+            this.label1.Size = new System.Drawing.Size(377, 51);
             this.label1.TabIndex = 3;
             this.label1.Text = "La inspiración para crear NagaPark vino de nuestro\r\namor hacia la cultura japones" +
     "a, mas concretamente\r\ndel anime (animación japonesa) Ijiranaide Nagatoro-San";
@@ -70,18 +70,20 @@
             // 
             this.lblPregunta2.AutoSize = true;
             this.lblPregunta2.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPregunta2.Location = new System.Drawing.Point(-2, 144);
+            this.lblPregunta2.Location = new System.Drawing.Point(-2, 117);
+            this.lblPregunta2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPregunta2.Name = "lblPregunta2";
-            this.lblPregunta2.Size = new System.Drawing.Size(546, 25);
+            this.lblPregunta2.Size = new System.Drawing.Size(433, 20);
             this.lblPregunta2.TabIndex = 2;
             this.lblPregunta2.Text = "¿Cual fue la inspiración para la creacion de NagaPark?";
             // 
             // lblRespuesta1
             // 
             this.lblRespuesta1.AutoSize = true;
-            this.lblRespuesta1.Location = new System.Drawing.Point(19, 58);
+            this.lblRespuesta1.Location = new System.Drawing.Point(14, 47);
+            this.lblRespuesta1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRespuesta1.Name = "lblRespuesta1";
-            this.lblRespuesta1.Size = new System.Drawing.Size(223, 80);
+            this.lblRespuesta1.Size = new System.Drawing.Size(185, 68);
             this.lblRespuesta1.TabIndex = 1;
             this.lblRespuesta1.Text = "Ernesto Josemaría Moretti\r\nGalo Hoyos Avilés\r\nSantiago Vittori\r\nYue Huang";
             // 
@@ -89,9 +91,10 @@
             // 
             this.lblPregunta1.AutoSize = true;
             this.lblPregunta1.Font = new System.Drawing.Font("MV Boli", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPregunta1.Location = new System.Drawing.Point(-4, 28);
+            this.lblPregunta1.Location = new System.Drawing.Point(-3, 23);
+            this.lblPregunta1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPregunta1.Name = "lblPregunta1";
-            this.lblPregunta1.Size = new System.Drawing.Size(560, 25);
+            this.lblPregunta1.Size = new System.Drawing.Size(443, 20);
             this.lblPregunta1.TabIndex = 0;
             this.lblPregunta1.Text = "¿Quiénes participaron en la creación de esta aplicación?";
             // 
@@ -99,10 +102,10 @@
             // 
             this.picFAQ.BackColor = System.Drawing.Color.Transparent;
             this.picFAQ.Image = global::Trabajo_POO_Grupo_4.Properties.Resources.faq;
-            this.picFAQ.Location = new System.Drawing.Point(33, 2);
-            this.picFAQ.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picFAQ.Location = new System.Drawing.Point(25, 2);
+            this.picFAQ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.picFAQ.Name = "picFAQ";
-            this.picFAQ.Size = new System.Drawing.Size(67, 53);
+            this.picFAQ.Size = new System.Drawing.Size(50, 43);
             this.picFAQ.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picFAQ.TabIndex = 4;
             this.picFAQ.TabStop = false;
@@ -111,10 +114,10 @@
             // 
             this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnCerrar.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Bold);
-            this.btnCerrar.Location = new System.Drawing.Point(229, 323);
-            this.btnCerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCerrar.Location = new System.Drawing.Point(172, 262);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(157, 59);
+            this.btnCerrar.Size = new System.Drawing.Size(118, 48);
             this.btnCerrar.TabIndex = 5;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
@@ -122,15 +125,16 @@
             // 
             // FAQ
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Trabajo_POO_Grupo_4.Properties.Resources.wallpaper;
-            this.ClientSize = new System.Drawing.Size(626, 394);
+            this.ClientSize = new System.Drawing.Size(470, 320);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.picFAQ);
             this.Controls.Add(this.PreguntasBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "FAQ";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
